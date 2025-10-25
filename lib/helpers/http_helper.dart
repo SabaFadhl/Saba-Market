cc2cf0cd8a5dc54b9694a6d1dcde62c56e92e07f
SabaFadhl <sabafadl98@gmail.com>
2025-10-26 00:09:40 +0300

    feat: add http helper class

---
+++ lib/helpers/http_helper.dart
@@ -0,0 +1,20 @@
+import 'package:dio/dio.dart';
+
+class HttpHelper{
+  Dio _dio = Dio();
+  HttpHelper._();
+  static HttpHelper? instance;
+  static HttpHelper get getInstance{
+    if(instance==null){
+      instance=HttpHelper._();
+    }
+    return instance!;
+  }
+
+  Future<Response> getRequest({required String url}) async {
+    return await _dio.get(url);
+  }
+  Future<Response> postRequest({required String url, required FormData data}){
+    return _dio.get(url);
+  }
+}
