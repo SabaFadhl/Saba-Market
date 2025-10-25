ee91472ae2cfd83980ef5a431a396a9ef255b89a
SabaFadhl <sabafadl98@gmail.com>
2025-10-26 00:09:30 +0300

    feat: add routes manager

---
+++ lib/helpers/routes_manager.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:saba_market/core/views/screens/home.dart';
+import 'package:saba_market/core/views/screens/login.dart';
+
+class RouteManager {
+  static Route<dynamic>? generateRoutes(RouteSettings settings) {
+    switch (settings.name) {
+      case "/home":
+        return MaterialPageRoute(builder: (ctx) => HomeScreen());
+      case "/login":
+        return MaterialPageRoute(builder: (ctx) => LoginScreen());
+      default:
+        return MaterialPageRoute(builder: (ctx) => Text("Not found"));
+    }
+  }
+}
