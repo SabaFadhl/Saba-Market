5d3aa489a381c87213fc8205fd7a985c8ac7be7c
SabaFadhl <sabafadl98@gmail.com>
2025-10-26 00:05:49 +0300

    feat: add app text field component

---
+++ lib/core/views/components/app_textformfield.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class AppTextformfield extends StatelessWidget {
+  AppTextformfield( {super.key, this.controller,required this.label, this.filled=true, this.isPassword=false, this.icon, this.horizontal=15});
+  String label;
+  bool filled;
+  bool isPassword;
+  Widget? icon;
+  double horizontal;
+  TextEditingController? controller;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: horizontal,vertical: 10),
+      child: TextFormField(
+        controller: controller,
+        obscureText: isPassword,
+        decoration: InputDecoration(
+          suffixIcon: icon,
+          labelText: label,
+          filled: true,
+          fillColor: filled ?Colors.grey[100]:null,
+          border: OutlineInputBorder(
+            // borderSide: BorderSide(color: Colors.teal, style: BorderStyle.solid, width: 55),
+            borderRadius: BorderRadius.circular(10),
+          ),
+        ),
+      ),
+    );
+  }
+}
