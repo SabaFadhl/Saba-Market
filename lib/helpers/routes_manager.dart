cb9d44248db17dce9d37b1a65d5e6d88b5890f3f
SabaFadhl <sabafadl98@gmail.com>
2025-10-26 01:21:30 +0300

    feat: add register to toute manager

--- lib/helpers/routes_manager.dart
+++ lib/helpers/routes_manager.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:saba_market/core/views/screens/home.dart';
 import 'package:saba_market/core/views/screens/login.dart';
+import 'package:saba_market/core/views/screens/register.dart';
 
 class RouteManager {
   static Route<dynamic>? generateRoutes(RouteSettings settings) {
@@ -9,6 +10,8 @@ class RouteManager {
         return MaterialPageRoute(builder: (ctx) => HomeScreen());
       case "/login":
         return MaterialPageRoute(builder: (ctx) => LoginScreen());
+      case "/register":
+        return MaterialPageRoute(builder: (ctx) => RegisterScreen());
       default:
         return MaterialPageRoute(builder: (ctx) => Text("Not found"));
     }
